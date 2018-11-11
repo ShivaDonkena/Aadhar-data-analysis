d48583c13e5f22bd0c5983a2d6acd51f30fb267e
shiva kumar <donkena.shiva@gmail.com>
2018-11-11 23:55:34 +0530

    reading file

--- AadharDataAnalysis/src/main/scala/com/aadhar/analysis/AadharDataFilter.scala
+++ AadharDataAnalysis/src/main/scala/com/aadhar/analysis/AadharDataFilter.scala
@@ -1,8 +1,19 @@
 package com.aadhar.analysis
 
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
 object AadharDataFilter {
 
   def main(args: Array[String]): Unit = {
-    print("Shiva")
+    val path: String = "A:\\bigdata\\UIDAI-ENR-DETAIL-20170308.csv"
+    val spark: SparkSession = SparkSession.builder().master("local[2]").appName("AadharAanalysis").getOrCreate()
+    val data: DataFrame = readFile(spark, path)
+
   }
+
+  def readFile(sparkSession: SparkSession, path: String): DataFrame = {
+    sparkSession.read.format("csv").option("header", true).load(path = path)
+  }
+
 }
+
